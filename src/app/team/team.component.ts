bc758018f1507d68364b7f8a1a8d990ba790d6e2
Emmanuel Adiba <eabaagah@gmail.com>
2022-06-05 17:27:55 +0000

    update team component

--- src/app/team/team.component.ts
+++ src/app/team/team.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-team',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./team.component.css']
 })
 export class TeamComponent implements OnInit {
+
+  @Input() team: string[] = []
+  @Input() index = 0
 
   constructor() { }
 
